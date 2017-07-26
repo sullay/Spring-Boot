f7fe478e02e0bdeb77260788fbc596283ed7a8b9
sullay <i-sullay@wicresoft.com>
2017-07-26 14:13:33 +0800

    事务管理

---
+++ Chapter3-3-1/src/main/java/com/sullay/Chapter322Application.java
@@ -0,0 +1,12 @@
+package com.sullay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter322Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter322Application.class, args);
+	}
+}
